a27def64c5cafef0bfb3b556efa6f4f3a8a7a635
yannis.perrot <yannis.perrot@cgi.com>
2019-03-08 23:45:03 +0100

    avancement

--- get/getTest.js
+++ get/getTest.js
@@ -1,9 +1,16 @@
-fetch('http://smashlounge.com/api/attack/char/10')
-    .then(response => response.json())
-    .then((data) => {
-        fs.writeFile(__dirname + '/data/test.json', JSON.stringify(data), function(err, result){
-            if (err) {
-                console.log("error writefile : ", err);
-            }
-        })
-    });
+const fetch = require('node-fetch');
+const fs = require('file-system');
+
+exports.data = function() {
+    for (let i = 1; i <= 26; i++) {
+        fetch('http://smashlounge.com/api/attack/char/' + i)
+            .then(response => response.json())
+            .then((data) => {
+                fs.writeFile(__dirname + '/data/' + i + '.json', JSON.stringify(data), function (err, result) {
+                    if (err) {
+                        console.log("error writefile : ", err);
+                    }
+                })
+            });
+    }
+}
